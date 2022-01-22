964794684203bfd788784604d323633ddd32b306
Ozan Sahin <kamilozan35@gmail.com>
2022-01-22 21:43:28 +0300

    Initial commit

---
+++ src/main/java/com/example/hazelcastcachingwithspringboot/HazelcastCachingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hazelcastcachingwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastCachingWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelcastCachingWithSpringBootApplication.class, args);
+    }
+
+}
